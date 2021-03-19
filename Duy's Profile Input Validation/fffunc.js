600ba8fbcecf0d695bd6aa25c8c06dc43df28643
duy24 <78519500+duy24@users.noreply.github.com>
2021-03-18 21:09:37 -0500

    Add files via upload

---
+++ Duy's Profile Input Validation/fffunc.js
@@ -0,0 +1,11 @@
+function calculateTotal() {
+    let x = $("#suggestedp").val() * $("#gallonsreq").val();
+    $("#totalamt").val(x);
+}
+
+$(function() {
+    $("#gallonsreq").on("change keyup", calculateTotal);
+})
+
+let today = new Date().toISOString().substr(0, 10);
+document.querySelector("#ddate").value = document.querySelector("#ddate").min = today;
---
+++ Duy's Profile Input Validation/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+const { check, validationResult } = require('express-validator')
+
+let urlencoded = express.urlencoded({extended: true})
+app.use(express.json());
+app.use(urlencoded);
+app.use(express.static(__dirname + '/'))
+
+app.get('/',(request, response) => {
+    response.sendFile(path.join(__dirname + '/profile_dv.html'))
+})
+
+app.post('/formData',[
+    check('fullname')
+    .not().isEmpty().withMessage('Name cannot be empty.')
+    .isLength({
+        max:50
+    }).withMessage('50 characters max for full name.')
+    .trim(),
+
+    //check('address')
+    //.not().isEmpty().withMessage('Address cannot be empty.'),
+    check('city')
+    .not().isEmpty().withMessage('City cannot be empty.')
+    .isLength({
+         max:100
+    }).withMessage('100 characters max for city.'),
+
+    check('zipcode')
+    .isInt().withMessage("Zipcode can only contain numbers.")
+    .isLength({
+        min:5,
+        max:9
+    }).withMessage('Minimum of 5 characters and Maximum of 9 characters for Zipcode.')
+] ,(request, response) => {
+    const errors = validationResult(request);
+
+    if(!errors.isEmpty()){
+        return response.status(422).json({errors: errors.array()});
+    }
+    response.status(202).json({
+        success:'Ok'
+    })
+})
+
+app.listen(port, () => console.log('server running'))
