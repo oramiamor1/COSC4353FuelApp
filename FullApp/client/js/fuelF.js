3571eb6dca3c51bc8b8db743dab85294cd8c6586
duy24 <78519500+duy24@users.noreply.github.com>
2021-04-28 10:11:52 -0500

    Update fuelF.js

--- FullApp/client/js/fuelF.js
+++ FullApp/client/js/fuelF.js
@@ -3,7 +3,26 @@ async function insertProf() {
     var deliveryadr = document.querySelector('#deliveryadr').value;
     var ddate = document.querySelector('#ddate').value;
     console.log(gallonsreq);
-  
+    /////
+    var state = document.querySelector('#state').value;
+
+    var locationFac = 0.04;
+    var historyFac = 0;
+    var gallonsreqFac = 0.03;
+    var profitFac = 0.1;
+
+    var currentPrice = 1.50;
+
+    if(state == 'Texas'){
+        locationFac = 0.02;
+    }
+
+    if (gallonsreq > 1000){
+        gallonsreqFac = 0.02;
+    }
+
+    var margin = currentPrice * (locationFac - historyFac + gallonsreqFac + profitFac);
+    ////////
     try {
     const body = {
         gallonsreq: gallonsreq, 
