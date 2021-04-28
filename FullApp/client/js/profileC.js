7a57a0e8ddc6ab5ebbdedc69c6ad19a7d4ad993e
duy24 <78519500+duy24@users.noreply.github.com>
2021-04-27 20:34:20 -0500

    Update profileC.js

--- FullApp/client/js/profileC.js
+++ FullApp/client/js/profileC.js
@@ -67,7 +67,7 @@ async function insertProf() {
         zip: zip
     };
 
-    const response = await fetch(`http://localhost:3000/profile`, {
+    const response = await fetch(`http://localhost:5000/profile`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
